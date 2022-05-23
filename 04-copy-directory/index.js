3c24a920bd9e9fdc7645721c98227cfe74fbe08c
ahentosmak <zhentosmak@mail.ru>
2022-05-24 01:09:41 +0300

    feat: add files in folder solutin

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+let copyFolderName = path.join(__dirname, 'files-copy');
+let folderName = path.join(__dirname, 'files');
+
+fs.mkdir(copyFolderName, { recursive: true }, (err) => {
+  if (err) console.log(err);
+});
+
+fs.readdir(folderName, { withFileTypes: true }, (err, files) => {
+  if (err) console.log(err);
+
+  async function copyFiles(files, allCopyFiles) {
+    await allCopyFiles.forEach((file) => {
+      let copyFileName = path.join(__dirname, 'files-copy', file);
+      fsPromises.unlink(copyFileName);
+    });
+
+    await files.forEach((file) => {
+      let fileName = path.join(__dirname, 'files', file.name);
+      let copyFileName = path.join(__dirname, 'files-copy', file.name);
+      fsPromises.copyFile(fileName, copyFileName);
+    });
+  }
+
+  fs.readdir(copyFolderName, (err, allCopyFiles) => {
+    if (err) console.log(err);
+    copyFiles(files, allCopyFiles);
+  });
+});
